f260f10d69de8f9e3acb40fc8b15f9eef9499a60
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2024-11-30 21:45:23 +0100

    rough outline of the client added

--- src/clients/client_danylo.rs
+++ src/clients/client_danylo.rs
@@ -1,9 +1,52 @@
-//To do (Two different ones)
-//Danyl
-use crossbeam_channel::{select, select_biased, Receiver, Sender};
-
-use std::fmt::Debug;
-use wg_2024::controller::Command;
-use wg_2024::drone::{Drone, DroneOptions};
+use crossbeam_channel::{Receiver, Sender};
 use wg_2024::network::NodeId;
-use wg_2024::packet::{Ack, FloodRequest, FloodResponse, Fragment, Nack, NackType, NodeType, Packet, PacketType, ServerType};
+use wg_2024::packet::Packet;
+
+struct ClientDanylo {
+    id: NodeId,
+    connected_drone_ids: Vec<NodeId>,
+    packet_send: Sender<Packet>,
+    packet_recv: Receiver<Packet>,
+}
+
+impl ClientDanylo {
+    fn new() {
+        todo!()
+    }
+
+    fn run(&mut self) {
+        todo!()
+    }
+
+    fn connect(&mut self) {
+        todo!()
+    }
+
+    fn send_message(&mut self) {
+        todo!()
+    }
+
+    fn receive_message(&mut self) {
+        todo!()
+    }
+
+    fn discover_network(&mut self) {
+        todo!()
+    }
+
+    fn serialize_message(&mut self) {
+        todo!()
+    }
+
+    fn fragment_message(&mut self) {
+        todo!()
+    }
+
+    fn reassemble_message(&mut self) {
+        todo!()
+    }
+
+    fn handle_nack(&mut self) {
+        todo!()
+    }
+}
