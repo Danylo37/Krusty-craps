ecc924d4bfd0ead5d33629db617e9766412a7dcc
Leonardo Colli <80243470+lillo24@users.noreply.github.com>
2024-12-17 11:32:11 +0100

    Update server.rs

--- src/servers/server.rs
+++ src/servers/server.rs
@@ -1,15 +1,14 @@
 //I am a god
 
 use crossbeam_channel::{select_biased, Receiver, Sender};
-use std::collections::{HashMap, HashSet};
-use std::fmt::Debug;
+use std::collections::{HashMap};
 use wg_2024::{
     network::{NodeId, SourceRoutingHeader},
     packet::{
         Ack, FloodRequest, FloodResponse, Fragment, Nack, NackType, NodeType, Packet, PacketType,
     },
 };
-use crate::general_use::{Message, Query, Response, ServerCommand, ServerEvent, ServerType};
+use crate::general_use::{Message, ServerCommand, ServerType};
 
 ///SERVER TRAIT
 pub trait Server{
@@ -24,6 +23,7 @@ pub trait Server{
     fn get_packet_send_not_mutable(&self) -> &HashMap<NodeId, Sender<Packet>>;
 
     fn get_reassembling_messages(&mut self) -> &mut HashMap<u64, Vec<u8>>;
+    fn get_sending_messages(&mut self) -> &mut HashMap<u64, (Vec<u8>, u8)>;
 
     fn run(&mut self){
         loop {
@@ -44,7 +44,7 @@ pub trait Server{
                 recv(self.get_packet_recv()) -> packet_res => {
                     if let Ok(packet) = packet_res {
                         match packet.pack_type {
-                            PacketType::Nack(nack) => self.handle_nack(nack),
+                            PacketType::Nack(nack) => self.handle_nack(nack, packet.session_id),
                             PacketType::Ack(ack) => self.handle_ack(ack),
                             PacketType::MsgFragment(fragment) => self.handle_fragment(fragment, packet.routing_header ,packet.session_id),
                             PacketType::FloodRequest(flood_request) => self.handle_flood_request(flood_request, packet.session_id),
@@ -88,8 +88,29 @@ pub trait Server{
     }
 
     //NACK
-    fn handle_nack(&mut self, nack: Nack){
-        todo!();
+    fn handle_nack(&mut self, nack: Nack, session_id: u64){
+        match nack.nack_type {
+            NackType::UnexpectedRecipient(node_id) => { //What is actually unexpected recipient? Can it even happen?
+                println!("Unexpected Recipient at node {}", node_id);
+                self.discover();
+                self.send_again_fragment(session_id, nack.fragment_index);
+            },
+            NackType::Dropped => {
+                println!("Dropped");
+                self.discover();
+                self.send_again_fragment(session_id, nack.fragment_index);
+            },
+            NackType::DestinationIsDrone => {
+                println!("Destination is drone?");
+                self.discover();
+                self.send_again_fragment(session_id, nack.fragment_index);
+            },
+            NackType::ErrorInRouting(node_id) => {
+                println!("Error in routing in node: {}", node_id);
+                self.discover();
+                self.send_again_fragment(session_id, nack.fragment_index);
+            }
+        }
     }
     fn send_nack(&self, p0: Nack, p1: SourceRoutingHeader, p2: u64){
         todo!();
@@ -123,7 +144,7 @@ pub trait Server{
     }
 
     fn find_path_to(&self, destination_id: NodeId) -> Vec<NodeId>{
-        todo!();
+        vec![4,2,1,3]
     }
 
     fn create_source_routing(route: Vec<NodeId>) -> SourceRoutingHeader{
@@ -165,17 +186,19 @@ pub trait Server{
                 // ... error handling logic ...
                 return;
 
-            }else{
+            }else if offset as u64 != fragment.fragment_index*128 {
+                logic error: Check if a fragment was skipped and decide if saving data anyway of next fragments waiting for the one fragment that will be sent again
 
-                println!("Copy");
-                // Copy data to the correct offset in the vector.
-                reassembling_message.extend_from_slice(&data_to_add);
+            }
+                else{
+                    println!("Copy");
+                    // Copy data to the correct offset in the vector.
+                    reassembling_message.extend_from_slice(&data_to_add);
 
-                // Check if all fragments have been received
-                let reassembling_message_clone = reassembling_message.clone();
-                println!("N fragments + current fragment length{}", fragment.total_n_fragments*128 + fragment.length as u64);
-                self.if_all_fragments_received_process(&reassembling_message_clone, &fragment, session_id, routing_header);
-
+                    // Check if all fragments have been received
+                    let reassembling_message_clone = reassembling_message.clone();
+                    println!("N fragments + current fragment length{}", fragment.total_n_fragments*128 + fragment.length as u64);
+                    self.if_all_fragments_received_process(&reassembling_message_clone, &fragment, session_id, routing_header);
             }
 
         }else {
@@ -201,7 +224,7 @@ pub trait Server{
 
         println!("Trying sending");
         println!("Message length {} n_fragments {} current.fragment length {}, fragment_index: {}", message.len(), current_fragment.total_n_fragments, current_fragment.length, current_fragment.fragment_index );
-        if((message.len() as u64) == ((current_fragment.total_n_fragments-1)*128 + current_fragment.length as u64))
+        if(message.len() as u64) == ((current_fragment.total_n_fragments-1)*128 + current_fragment.length as u64)
         {
             println!("Sending back");
             let reassembled_data = message.clone(); // Take ownership of the data
@@ -218,14 +241,15 @@ pub trait Server{
         false
     }
     fn process_reassembled_message(&mut self, data: Vec<u8>, src_id: NodeId);
-    fn send_fragments(&self, session_id: u64, n_fragments: usize, response_in_vec_bytes: &[u8], header: SourceRoutingHeader) {
-
+    fn send_fragments(&mut self, session_id: u64, n_fragments: usize, response_in_vec_bytes: &[u8], header: SourceRoutingHeader) {
+        self.get_sending_messages().insert(session_id, (response_in_vec_bytes.to_vec(), header.destination().unwrap()));
         for i in 0..n_fragments{
 
             //Preparing data of fragment
             let mut data:[u8;128] = [0;128];
-            if((i+1)*128>response_in_vec_bytes.len()){
-                data[0..response_in_vec_bytes.len()].copy_from_slice(&response_in_vec_bytes[i*128..response_in_vec_bytes.len()]);
+            if(i+1)*128>response_in_vec_bytes.len(){
+                data[0..(response_in_vec_bytes.len()-(i*128))].copy_from_slice(&response_in_vec_bytes[i*128..response_in_vec_bytes.len()]);
+
             }else{
                 data.copy_from_slice(&response_in_vec_bytes[i*128..(1+i)*128]);
             }
@@ -248,7 +272,47 @@ pub trait Server{
         }
     }
 
+    fn send_again_fragment(&mut self, session_id: u64, fragment_index: u64){
 
+        //Getting right message and destination id
+        let message_and_destination = self.get_sending_messages().get(&session_id).unwrap();
+
+        //Preparing fields for Fragment
+        let length_response = message_and_destination.0.len();
+        let mut n_fragments = length_response / 128+1;
+        if n_fragments == 0 {
+            n_fragments -= 1;
+        }
+
+        let offset = fragment_index*128;
+        let mut data:[u8;128] = [0;128];
+        if offset > length_response as u64 {
+            data[0..(message_and_destination.0.len() as u64 - offset)].copy_from_slice(&message_and_destination.0[offset as usize..message_and_destination.0.len()]);
+        }else{
+            data.copy_from_slice(&message_and_destination.0[offset..(offset+128)]);
+        }
+
+
+        //Generating fragment
+        let fragment = Fragment::new(
+            fragment_index,
+            n_fragments as u64,
+            data,
+        );
+
+        //Finding route
+        let route = self.find_path_to(message_and_destination.1);
+
+        //Generating packet
+        let packet = Self::create_packet(
+            PacketType::MsgFragment(fragment),
+            Self::create_source_routing(route),
+            session_id,
+        );
+
+        self.send_packet(packet);
+
+    }
     //Common functions
     fn give_type_back(&mut self, src_id: NodeId){
         println!("We did it");
