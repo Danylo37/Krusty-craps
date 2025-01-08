afec76a30dedeb84eec4fcf6745b0c9c5bcd5f7e
Leonardo Colli <80243470+lillo24@users.noreply.github.com>
2025-01-08 18:09:19 +0100

    Update media_server.rs

--- src/servers/media_server.rs
+++ src/servers/media_server.rs
@@ -1,5 +1,5 @@
 use crossbeam_channel::{Receiver, Sender};
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 use std::fmt::Debug;
 use wg_2024::{
     network::{NodeId},
@@ -8,21 +8,31 @@ use wg_2024::{
     },
 };
 use crate::general_use::{Query, Response, ServerCommand, ServerEvent, ServerType};
-
+use crate::general_use::ServerError::UnexpectedError;
 use super::server::MediaServer as CharTrait;
 use super::server::Server as MainTrait;
 
 use super::content::IMAGE_PATHS;
 
+type FloodId = u64;
+type SessionId = u64;
 #[derive(Debug)]
 pub struct MediaServer{
 
     //Basic data
     pub id: NodeId,
     pub connected_drone_ids: Vec<NodeId>,
-    pub flood_ids: HashSet<u64>,
-    pub reassembling_messages: HashMap<u64, Vec<u8>>,
-    pub counter: (u64, u64),
+
+    //Fragment-related
+    pub reassembling_messages: HashMap<SessionId, Vec<u8>>,
+    pub sending_messages: HashMap<SessionId, (Vec<u8>, NodeId)>,
+
+    //Flood-related
+    pub clients: Vec<NodeId>,                                   // Available clients
+    pub topology: HashMap<NodeId, Vec<NodeId>>,             // Nodes and their neighbours
+    pub routes: HashMap<NodeId, Vec<NodeId>>,                   // Routes to the servers
+    pub flood_ids: Vec<FloodId>,
+    pub counter: (FloodId, SessionId),
 
     //Channels
     pub to_controller_event: Sender<ServerEvent>,
@@ -47,8 +57,14 @@ impl MediaServer{
         MediaServer {
             id,
             connected_drone_ids,
-            flood_ids: Default::default(),
+
             reassembling_messages: Default::default(),
+            sending_messages: Default::default(),
+
+            clients: Default::default(),                                   // Available clients
+            topology: Default::default(),
+            routes: Default::default(),
+            flood_ids: Default::default(),
             counter: (0, 0),
 
             to_controller_event,
@@ -87,22 +103,30 @@ impl MediaServer{
 
 impl MainTrait for MediaServer{
     fn get_id(&self) -> NodeId{ self.id }
-
     fn get_server_type(&self) -> ServerType{ ServerType::Media }
-    fn get_flood_id(&mut self) -> u64{
-        self.counter.0 += 1;
-        self.counter.0
-    }
+
     fn get_session_id(&mut self) -> u64{
         self.counter.1 += 1;
         self.counter.1
     }
-    fn get_from_controller_command(&mut self) -> &mut Receiver<ServerCommand>{ &mut self.from_controller_command }
 
+    fn get_flood_id(&mut self) -> u64{
+        self.counter.0 += 1;
+        self.counter.0
+    }
+
+    fn push_flood_id(&mut self, flood_id: FloodId){ self.flood_ids.push(flood_id); }
+    fn get_clients(&mut self) -> &mut Vec<NodeId>{ &mut self.clients }
+    fn get_topology(&mut self) -> &mut HashMap<NodeId, Vec<NodeId>>{ &mut self.topology }
+    fn get_routes(&mut self) -> &mut HashMap<NodeId, Vec<NodeId>>{ &mut self.routes }
+
+
+    fn get_from_controller_command(&mut self) -> &mut Receiver<ServerCommand>{ &mut self.from_controller_command }
     fn get_packet_recv(&mut self) -> &mut Receiver<Packet>{ &mut self.packet_recv }
     fn get_packet_send(&mut self) -> &mut HashMap<NodeId, Sender<Packet>>{ &mut self.packet_send }
     fn get_packet_send_not_mutable(&self) -> &HashMap<NodeId, Sender<Packet>>{ &self.packet_send }
     fn get_reassembling_messages(&mut self) -> &mut HashMap<u64, Vec<u8>>{ &mut self.reassembling_messages }
+    fn get_sending_messages(&mut self) ->  &mut HashMap<u64, (Vec<u8>, u8)>{ &mut self.sending_messages }
 
     fn process_reassembled_message(&mut self, data: Vec<u8>, src_id: NodeId){
         match String::from_utf8(data.clone()) {
@@ -115,9 +139,8 @@ impl MainTrait for MediaServer{
                 }
                 _ => {}
             },
-            Err(e) => println!("Dio porco, {:?}", e),
+            Err(e) => println!("Argh, {:?}", e),
         }
-        println!("process reassemble finished");
     }
 }
 
@@ -132,7 +155,7 @@ impl CharTrait for MediaServer{
         if let Some(media) = media {
             response = Response::Media(media.clone());
         }else{
-            response = Response::Err("Media not found".to_string());
+            response = Response::Err(UnexpectedError("Media not found".to_string()));
         }
 
         //Serializing message to send
@@ -158,3 +181,4 @@ impl CharTrait for MediaServer{
 
     }
 }
+
