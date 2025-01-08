85dbf02c7e69a5d08a2a219620fc16518bd2afe8
Leonardo Colli <80243470+lillo24@users.noreply.github.com>
2025-01-08 18:10:14 +0100

    Update communication_server.rs

--- src/servers/communication_server.rs
+++ src/servers/communication_server.rs
@@ -1,6 +1,7 @@
 use crossbeam_channel::{Receiver, Sender};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fmt::Debug;
+//use eframe::egui::accesskit::NodeId as ui_node_id;
 use wg_2024::{
     network::{NodeId},
     packet::{
@@ -12,16 +13,26 @@ use crate::general_use::{Message, Query, Response, ServerCommand, ServerEvent, S
 use super::server::CommunicationServer as CharTrait;
 use super::server::Server as MainTrait;
 
+type FloodId = u64;
+type SessionId = u64;
+
 #[derive(Debug)]
 pub struct CommunicationServer{
 
     //Basic data
     pub id: NodeId,
     pub connected_drone_ids: Vec<NodeId>,
-    pub flood_ids: HashSet<u64>,
-    pub reassembling_messages: HashMap<u64, Vec<u8>>,
-    pub sending_messages: HashMap<u64, Vec<u8>>,
-    pub counter: (u64, u64),
+
+    //Fragment-related
+    pub reassembling_messages: HashMap<SessionId, Vec<u8>>,
+    pub sending_messages: HashMap<SessionId, (Vec<u8>, NodeId)>,
+
+    //Flood-related
+    pub clients: Vec<NodeId>,                                   // Available clients
+    pub topology: HashMap<NodeId, Vec<NodeId>>,             // Nodes and their neighbours
+    pub routes: HashMap<NodeId, Vec<NodeId>>,                   // Routes to the servers
+    pub flood_ids: Vec<FloodId>,
+    pub counter: (FloodId, SessionId),
 
     //Channels
     pub to_controller_event: Sender<ServerEvent>,
@@ -30,7 +41,7 @@ pub struct CommunicationServer{
     pub packet_send: HashMap<NodeId, Sender<Packet>>,
 
     //Characteristic-Server fields
-    pub list_users: HashMap<String, NodeId>,
+    pub list_users: Vec<NodeId>,
 }
 
 impl CommunicationServer{
@@ -45,9 +56,14 @@ impl CommunicationServer{
         CommunicationServer {
             id,
             connected_drone_ids,
-            flood_ids: Default::default(),
+
             reassembling_messages: Default::default(),
             sending_messages: Default::default(),
+
+            clients: Default::default(),                                   // Available clients
+            topology: Default::default(),
+            routes: Default::default(),
+            flood_ids: Default::default(),
             counter: (0, 0),
 
             to_controller_event,
@@ -55,29 +71,36 @@ impl CommunicationServer{
             packet_recv,
             packet_send,
 
-            list_users: HashMap::new(),
+            list_users: Vec::new(),
         }
     }
 }
 
 impl MainTrait for CommunicationServer{
     fn get_id(&self) -> NodeId{ self.id }
-
     fn get_server_type(&self) -> ServerType{ ServerType::Communication }
-    fn get_flood_id(&mut self) -> u64{
-        self.counter.0 += 1;
-        self.counter.0
-    }
+
     fn get_session_id(&mut self) -> u64{
         self.counter.1 += 1;
         self.counter.1
     }
+
+    fn get_flood_id(&mut self) -> u64{
+        self.counter.0 += 1;
+        self.counter.0
+    }
+
+    fn push_flood_id(&mut self, flood_id: FloodId){ self.flood_ids.push(flood_id); }
+    fn get_clients(&mut self) -> &mut Vec<NodeId>{ &mut self.clients }
+    fn get_topology(&mut self) -> &mut HashMap<NodeId, Vec<NodeId>>{ &mut self.topology }
+    fn get_routes(&mut self) -> &mut HashMap<NodeId, Vec<NodeId>>{ &mut self.routes }
+
     fn get_from_controller_command(&mut self) -> &mut Receiver<ServerCommand>{ &mut self.from_controller_command }
     fn get_packet_recv(&mut self) -> &mut Receiver<Packet>{ &mut self.packet_recv }
     fn get_packet_send(&mut self) -> &mut HashMap<NodeId, Sender<Packet>>{ &mut self.packet_send }
     fn get_packet_send_not_mutable(&self) -> &HashMap<NodeId, Sender<Packet>>{ &self.packet_send }
     fn get_reassembling_messages(&mut self) -> &mut HashMap<u64, Vec<u8>>{ &mut self.reassembling_messages }
-    fn get_sensing_messages(&mut self) -> &mut HashMap<u64, Vec<u8>>{ &mut self.sending_messages }
+    fn get_sending_messages(&mut self) ->  &mut HashMap<u64, (Vec<u8>, u8)>{ &mut self.sending_messages }
 
 
     fn process_reassembled_message(&mut self, data: Vec<u8>, src_id: NodeId){
@@ -85,24 +108,23 @@ impl MainTrait for CommunicationServer{
             Ok(data_string) => match serde_json::from_str(&data_string) {
                 Ok(Query::AskType) => self.give_type_back(src_id),
 
-                Ok(Query::AddClient(nickname, node_id)) => self.add_client(nickname, node_id),
+                Ok(Query::RegisterClient(node_id)) => self.add_client(node_id),
                 Ok(Query::AskListClients) => self.give_list_back(src_id),
-                Ok(Query::SendMessageTo(nickname, message)) => self.forward_message_to(nickname, message),
+                Ok(Query::SendMessageTo(node_id, message)) => self.forward_message_to(node_id, message),
                 Err(_) => {
                     panic!("Damn, not the right struct")
                 }
                 _ => {}
             },
-                Err(e) => println!("Dio porco, {:?}", e),
+                Err(e) => println!("Argh, {:?}", e),
             }
-        println!("process reassemble finished");
     }
 
 }
 
 impl CharTrait for CommunicationServer {
     fn add_client(&mut self, client_id: NodeId) {
-        self.list_users.insert(client_id);
+        self.list_users.push(client_id);
 
         let response = Response::ClientRegistered;
 
@@ -118,8 +140,8 @@ impl CharTrait for CommunicationServer {
         }
 
         //Generating header
-        let route: Vec<NodeId> = self.find_path_to(client_id); //To implement findpath
-        let header = Self::create_source_routing(route); //To fill
+        let route: Vec<NodeId> = self.find_path_to(client_id);
+        let header = Self::create_source_routing(route);
 
         // Generating ids
         let session_id = self.generate_unique_session_id();
@@ -131,10 +153,10 @@ impl CharTrait for CommunicationServer {
     fn give_list_back(&mut self, client_id: NodeId) {
 
         //Get list
-        let keys_list_clients = self.list_users.keys().cloned().collect();
+        let list_clients = self.list_users.clone();
 
         //Creating data to send
-        let response = Response::ListUsers(keys_list_clients);
+        let response = Response::ListClients(list_clients);
 
         //Serializing message to send
         let response_as_string = serde_json::to_string(&response).unwrap();
@@ -148,8 +170,8 @@ impl CharTrait for CommunicationServer {
         }
 
         //Generating header
-        let route: Vec<NodeId> = self.find_path_to(client_id); //To implement findpath
-        let header = Self::create_source_routing(route); //To fill
+        let route: Vec<NodeId> = self.find_path_to(client_id);
+        let header = Self::create_source_routing(route);
 
         // Generating ids
         let session_id = self.generate_unique_session_id();
@@ -159,10 +181,10 @@ impl CharTrait for CommunicationServer {
 
     }
 
-    fn forward_message_to(&mut self, nickname: String, message: Message) {
+    fn forward_message_to(&mut self, destination_id: NodeId, message: Message) {
 
         //Creating data to send
-        let response = Response::MessageFrom(nickname.clone(),message);
+        let response = Response::MessageFrom(destination_id,message);
 
         //Serializing message to send
         let response_as_string = serde_json::to_string(&response).unwrap();
@@ -176,7 +198,7 @@ impl CharTrait for CommunicationServer {
         }
 
         //Generating header
-        let route: Vec<NodeId> = self.find_path_to(*self.list_users.get(&nickname).unwrap());
+        let route: Vec<NodeId> = self.find_path_to(*self.list_users.get(&destination_id).unwrap());
         let header = Self::create_source_routing(route);
 
         // Generating fragment
@@ -185,3 +207,4 @@ impl CharTrait for CommunicationServer {
         self.send_fragments(session_id, n_fragments,response_in_vec_bytes, header);
     }
 }
+
