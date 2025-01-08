c0a7f162184b6a5145c71bbd08fd453c52f9749d
Leonardo Colli <80243470+lillo24@users.noreply.github.com>
2025-01-08 18:10:45 +0100

    Update text_server.rs

--- src/servers/text_server.rs
+++ src/servers/text_server.rs
@@ -1,28 +1,38 @@
-use crossbeam_channel::{select_biased, Receiver, Sender};
-use std::collections::{HashMap, HashSet};
+use crossbeam_channel::{Receiver, Sender};
+use std::collections::{HashMap};
 use std::fmt::Debug;
 use wg_2024::{
-    network::{NodeId, SourceRoutingHeader},
+    network::{NodeId},
     packet::{
-        Ack, FloodRequest, FloodResponse, Fragment, Nack, NackType, NodeType, Packet, PacketType,
+        Packet,
     },
 };
-use crate::general_use::{Message, Query, Response, ServerCommand, ServerEvent, ServerType};
+use crate::general_use::{Query, Response, ServerCommand, ServerEvent, ServerType};
 
 use super::server::TextServer as CharTrait;
 use super::server::Server as MainTrait;
 
 use super::content::TEXT;
 
+type FloodId = u64;
+type SessionId = u64;
 #[derive(Debug)]
 pub struct TextServer{
 
     //Basic data
     pub id: NodeId,
     pub connected_drone_ids: Vec<NodeId>,
-    pub flood_ids: HashSet<u64>,
-    pub reassembling_messages: HashMap<u64, Vec<u8>>,
-    pub counter: (u64, u64),
+
+    //Fragment-related
+    pub reassembling_messages: HashMap<SessionId, Vec<u8>>,
+    pub sending_messages: HashMap<SessionId, (Vec<u8>, NodeId)>,
+
+    //Flood-related
+    pub clients: Vec<NodeId>,                                   // Available clients
+    pub topology: HashMap<NodeId, Vec<NodeId>>,             // Nodes and their neighbours
+    pub routes: HashMap<NodeId, Vec<NodeId>>,                   // Routes to the servers
+    pub flood_ids: Vec<FloodId>,
+    pub counter: (FloodId, SessionId),
 
     //Channels
     pub to_controller_event: Sender<ServerEvent>,
@@ -44,11 +54,18 @@ impl TextServer{
         packet_send: HashMap<NodeId, Sender<Packet>>,
     ) -> Self {
         let content = Self::prepare_content();
+
         TextServer {
             id,
             connected_drone_ids,
-            flood_ids: Default::default(),
+
             reassembling_messages: Default::default(),
+            sending_messages: Default::default(),
+
+            clients: Default::default(),                                   // Available clients
+            topology: Default::default(),
+            routes: Default::default(),
+            flood_ids: Default::default(),
             counter: (0, 0),
 
             to_controller_event,
@@ -56,7 +73,7 @@ impl TextServer{
             packet_recv,
             packet_send,
 
-            content: Vec::new(),
+            content,
         }
     }
     fn prepare_content() -> Vec<String>{
@@ -85,22 +102,29 @@ impl TextServer{
 
 impl MainTrait for TextServer{
     fn get_id(&self) -> NodeId{ self.id }
-
     fn get_server_type(&self) -> ServerType{ ServerType::Text }
-    fn get_flood_id(&mut self) -> u64{
-        self.counter.0 += 1;
-        self.counter.0
-    }
+
     fn get_session_id(&mut self) -> u64{
         self.counter.1 += 1;
         self.counter.1
     }
-    fn get_from_controller_command(&mut self) -> &mut Receiver<ServerCommand>{ &mut self.from_controller_command }
 
+    fn get_flood_id(&mut self) -> u64{
+        self.counter.0 += 1;
+        self.counter.0
+    }
+
+    fn push_flood_id(&mut self, flood_id: FloodId){ self.flood_ids.push(flood_id); }
+    fn get_clients(&mut self) -> &mut Vec<NodeId>{ &mut self.clients }
+    fn get_topology(&mut self) -> &mut HashMap<NodeId, Vec<NodeId>>{ &mut self.topology }
+    fn get_routes(&mut self) -> &mut HashMap<NodeId, Vec<NodeId>>{ &mut self.routes }
+
+    fn get_from_controller_command(&mut self) -> &mut Receiver<ServerCommand>{ &mut self.from_controller_command }
     fn get_packet_recv(&mut self) -> &mut Receiver<Packet>{ &mut self.packet_recv }
     fn get_packet_send(&mut self) -> &mut HashMap<NodeId, Sender<Packet>>{ &mut self.packet_send }
     fn get_packet_send_not_mutable(&self) -> &HashMap<NodeId, Sender<Packet>>{ &self.packet_send }
     fn get_reassembling_messages(&mut self) -> &mut HashMap<u64, Vec<u8>>{ &mut self.reassembling_messages }
+    fn get_sending_messages(&mut self) ->  &mut HashMap<u64, (Vec<u8>, u8)>{ &mut self.sending_messages }
 
     fn process_reassembled_message(&mut self, data: Vec<u8>, src_id: NodeId){
         match String::from_utf8(data.clone()) {
@@ -115,9 +139,8 @@ impl MainTrait for TextServer{
                 }
                 _ => {}
             },
-            Err(e) => println!("Dio porco, {:?}", e),
+            Err(e) => println!("Argh, {:?}", e),
         }
-        println!("process reassemble finished");
     }
 }
 
