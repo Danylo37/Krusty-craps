53696816ebfcc3c770dc816ad5539e799691b6d3
Leonardo Colli <80243470+lillo24@users.noreply.github.com>
2025-01-10 00:56:49 +0100

    Update content.rs

--- src/servers/content.rs
+++ src/servers/content.rs
@@ -1,4 +1,47 @@
+use std::collections::HashMap;
+use rand::random;
+
+pub fn choose_random_texts() -> Vec<(u8, String)>{
+    let trying_closures = |x:u8| {
+        if x < 4 {
+            return x+3
+        }else{
+            x
+        }
+    };
+
+    let n_files = trying_closures(random::<u8>()%10);
+
+    let mut vec_files = Vec::new();
+    if(random::<u8>()%2 == 0){
+        for i in 0..n_files{
+            vec_files.push((i,TEXT[i].clone()));
+        }
+    }else{
+        for i in (0..n_files).rev(){
+            vec_files.push((i,TEXT[i].clone()));
+        }
+    }
+
+}
+
+pub fn get_media(vec_files: Vec<(u8, String)>) -> HashMap<String, String>{
+    IMAGE_PATHS.iter().filter_map(|(index_media, ref_s, media)| {
+        // Check if the `index_media` exists in `vec_files`
+        if vec_files.iter().any(|(index_chose, _)| *index_chose == *index_media) {
+            // Return the tuple (ref_s, media) if the condition is true
+            Some((ref_s.clone(), media.clone()))
+        } else {
+            // Discard the element by returning None
+            None
+        }
+    }).collect::<HashMap<String, String>>() // Collect into HashMap
+}
+
+
 pub const TEXT: [&str; 6] = [
+
+    //1
     r#"Alcuni versi di Leopardi:
 Ma perchè dare al sole,
 Perchè reggere in vita
@@ -10,19 +53,24 @@ E’ lo stato mortale.
 Ma tu mortal non sei,
 E forse del mio dir poco ti cale."#,
 
+    //2
     "Una banana #Media[banana]",
 
+    //3
     "Non scegliere questo testo #Media[do_not_search_this]",
 
+    //4
     r#"Phrases by Lillo:
 - a lack of belief in free will is the antidote to hate and judgement
 - il disordine è tale finche non viene ordinato
 - if you have to ask if you’re a member of a group, you’re probably not."#,
 
+    //5
     r#"One of the best panoramas are next to us,
 just walk up on a mountain,
 sit in the middle of the forest and watch at the Sparkling snow #Media[sparkling_snow]"#,
 
+    //6
     r#"Bigfoot Sighting Report
 Location: Dense forest near Willow Creek, California
 Date and Time: December 12, 2024, 4:45 PM
@@ -36,15 +84,9 @@ Its body appeared to be covered in dark, shaggy hair, likely black or very dark 
 ];
 
 
-pub const IMAGE_PATHS: [(&str, &str); 10] =  [
-    ("image1", "path/to/image1.jpeg"),
-    ("image2", "path/to/image2.jpeg"),
-    ("image3", "path/to/image3.jpeg"),
-    ("image4", "path/to/image4.jpeg"),
-    ("image5", "path/to/image5.jpeg"),
-    ("image6", "path/to/image6.jpeg"),
-    ("image7", "path/to/image7.jpeg"),
-    ("image8", "path/to/image8.jpeg"),
-    ("image9", "path/to/image9.jpeg"),
-    ("image10", "path/to/image10.jpeg"),
+pub const IMAGE_PATHS: [(u8, &str, &str); 4] =  [
+    (1, "banana", "path/to/banana.jpeg"),
+    (2, "do_not_search_this", "path/to/do_not_search_this.jpeg"),
+    (4, "sparkling_snow", "path/to/sparkling_snow.jpeg"),
+    (5, "big_foot", "path/to/big_foot.jpeg"),
 ];
