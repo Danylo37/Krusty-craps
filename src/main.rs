ea768b56f89532158dd80fcc195e871545981316
Leonardo Colli <80243470+lillo24@users.noreply.github.com>
2025-01-09 22:54:10 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ mod ui;
 
 mod clients;
 
+fn main() {
+    let mut my_net = network_initializer::NetworkInitializer::new();
+    my_net.initialize_from_file("input.toml");
+}
+
+/*
 use std::collections::{HashMap, HashSet};
 use crossbeam_channel::unbounded;
 use wg_2024::network::NodeId;
@@ -15,7 +21,8 @@ use crate::clients::client_chen::{ClientChen, NodeType};
 //Usages
 //use crate::network_initializer::NetworkInitializer;
 
-fn main() {
+
+fn monitoring_clients(){
     let (_packet_send_tx, packet_send_rx) = unbounded();
     let (client_send_tx, _controller_recv_rx) = unbounded();
     let (_controller_send_tx, client_recv_rx) = unbounded();
@@ -38,3 +45,5 @@ fn main() {
         Box::new(|_cc| Ok(Box::new(client))),
     ).unwrap();
 }
+
+*/
