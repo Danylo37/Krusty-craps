102154481db3cb606e8e9629a26b72ea234dda90
MuchoEnthusiast <tech.dev.cs.ac@gmail.com>
2025-01-11 17:24:37 +0100

    fix async things

--- src/ui_traits.rs
+++ src/ui_traits.rs
@@ -1,5 +1,9 @@
 use tokio::sync::mpsc;
+use std::future::Future;
 
-pub trait Monitoring{
-    async fn run_with_monitoring(&mut self, sender_to_gui: mpsc::Sender<Vec<u8>>);
+pub trait Monitoring {
+    fn run_with_monitoring(
+        &mut self,
+        sender_to_gui: mpsc::Sender<Vec<u8>>,
+    ) -> impl Future<Output = ()> + Send;
 }
